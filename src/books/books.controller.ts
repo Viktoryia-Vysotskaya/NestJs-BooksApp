64e4b7696ecea31b5bd72f27934f777d521673b3
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-12-09 20:30:18 +0100

    Add methods to fetch a single author and book by ID, and create GET endpoints for /api/authors/:id and /api/books/:id

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { ParseUUIDPipe } from '@nestjs/common';
 
 @Controller('books')
 export class BooksController {
@@ -8,5 +9,12 @@ export class BooksController {
   @Get('/')
   getAll() {
     return this.booksService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const book = await this.booksService.getById(id);
+    if (!book) throw new NotFoundException('Book not found');
+    return book;
   }
 }
