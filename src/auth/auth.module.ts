c5740146fbc2b7b50d45b02656ebfa70c885163b
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-12-10 10:05:38 +0100

    Implement middleware guard for login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
   imports: [UsersModule],
 })
-
 export class AuthModule {}
